e99b44ca72bf83ccbe5059a3cf2e8489e870f88f
simontjw <ge78dah@mytum.de>
2022-12-15 16:01:41 +0100

    fix: hotfix for complete

--- src/Express/Service/ExpressService.php
+++ src/Express/Service/ExpressService.php
@@ -749,9 +749,9 @@ class ExpressService
             $salesChannelContext->getContext()
         );
         $paymentHandlerData = \json_decode((string)$response->getContent(), true);
-        $redirectUrl = \stripslashes($paymentHandlerData['redirectUrl'] ?? 'reload');
+        $redirectUrl = \stripslashes($paymentHandlerData['redirectUrl'] ?? '');
         $this->logger->info('redirectUrl: ' . $redirectUrl);
-        $paymentToken = $this->getToken($$paymentHandlerData['redirectUrl'] ?? '');
+        $paymentToken = $this->getToken($redirectUrl);
         $this->logger->info('paymentToken: ' . $paymentToken);
         $this->savePaymentToken($ivyPaymentSession->getId(), $paymentToken, $salesChannelContext);
         $orderData['_sw_payment_token'] = $paymentToken;
